80ab3aaa8b41484e09213fb3dfa93bc94c86ba39
Roman Heinrich <roman.heinrich@gmail.com>
2016-01-10 22:44:50 +0100

    moves macros out of __using__ callback
    problem: 
    - the documentation for macros is swallowed because they are defined directly in __using__ callback
    
    solution: 
    - move them out to the Rop module directly

--- lib/rop.ex
+++ lib/rop.ex
@@ -4,64 +4,7 @@
 defmodule Rop do
   defmacro __using__(_) do
     quote do
-      # include normal functions
       import Rop
-
-      @doc ~s"""
-        No need to stop pipelining in case of an error somewhere in the middle
-      """
-      defmacro left >>> right do
-        quote do
-          (fn ->
-            case unquote(left) do
-              {:ok, x} -> x |> unquote(right)
-              {:error, _} = expr -> expr
-            end
-          end).()
-        end
-      end
-
-      @doc ~s"""
-        Wraps a simple function to return a tagged tuple with `:ok` to comply to the protocol `{:ok, result}`
-      """
-      defmacro bind(args, func) do
-        quote do
-          (fn ->
-            result = unquote(args) |> unquote(func)
-            {:ok, result}
-          end).()
-        end
-      end
-
-      @doc ~s"""
-        Wraps raising functions to return a tagged tuple `{:error, ErrorMessage} to comply with the protocol
-      """
-      defmacro try_catch(args, func) do
-        quote do
-          (fn ->
-            try do
-              unquote(args) |> unquote(func)
-            rescue
-              e -> {:error, e}
-            end
-          end).()
-        end
-      end
-
-
-
-      @doc ~s"""
-        Like a similar Unix utility it does some work and returns the input.
-        See [tee (command), Unix](https://en.wikipedia.org/wiki/Tee_(command)).
-      """
-      defmacro tee(args, func) do
-        quote do
-          (fn ->
-            unquote(args) |> unquote(func)
-            {:ok, unquote(args)}
-          end).()
-        end
-      end
     end
   end
 
@@ -84,4 +27,61 @@ defmodule Rop do
   def ok({:ok, x}), do: x
   def ok({:error, x}), do: raise x
   def ok(x), do: raise x
+
+
+  @doc ~s"""
+    No need to stop pipelining in case of an error somewhere in the middle
+  """
+  defmacro left >>> right do
+    quote do
+      (fn ->
+        case unquote(left) do
+          {:ok, x} -> x |> unquote(right)
+          {:error, _} = expr -> expr
+        end
+      end).()
+    end
+  end
+
+  @doc ~s"""
+    Wraps a simple function to return a tagged tuple with `:ok` to comply to the protocol `{:ok, result}`
+  """
+  defmacro bind(args, func) do
+    quote do
+      (fn ->
+        result = unquote(args) |> unquote(func)
+        {:ok, result}
+      end).()
+    end
+  end
+
+  @doc ~s"""
+    Wraps raising functions to return a tagged tuple `{:error, ErrorMessage}` to comply with the protocol
+  """
+  defmacro try_catch(args, func) do
+    quote do
+      (fn ->
+        try do
+          unquote(args) |> unquote(func)
+        rescue
+          e -> {:error, e}
+        end
+      end).()
+    end
+  end
+
+
+
+  @doc ~s"""
+    Like a similar Unix utility it does some work and returns the input.
+    See [tee (command), Unix](https://en.wikipedia.org/wiki/Tee_(command)).
+  """
+  defmacro tee(args, func) do
+    quote do
+      (fn ->
+        unquote(args) |> unquote(func)
+        {:ok, unquote(args)}
+      end).()
+    end
+  end
 end
